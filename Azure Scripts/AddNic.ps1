194fe12f27b2b2dbc5c419d6f88af23ecb416ec8
kblackstone <keith.blackstone@gmail.com>
2017-04-07 08:17:07 -0400

    Add files via upload

---
+++ Azure Scripts/AddNic.ps1
@@ -0,0 +1,11 @@
+﻿Login-AzureRMAccount
+Get-AzureRmNetworkInterface
+
+$subscriptionname = Read-Host -Prompt "Enter your Subscription ID"
+$rgname = Read-Host -Prompt "Enter Resource Group name"
+$vmname = Read-Host -Prompt "Enter the VM name"
+$nicname = Read-Host -Prompt "Enter the new NIC name"
+
+$myvm = Get-AzureRmVM -ResourceGroupName $rgname -VMName $vmname
+Add-AzureRmVMNetworkInterface -VM $myvm -Id "/subscriptions/$subscriptionname/resourceGroups/$rgname/providers/Microsoft.Network/networkInterfaces/$nicname"
+Update-AzureRmVM -ResourceGroupName $rgname -VM $myvm
