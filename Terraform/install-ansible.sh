93eb290a261a30bf441d1aeb5b44cba7cceedffe
kblackstone <keith.blackstone@gmail.com>
2017-08-18 14:19:55 -0400

    Update install-ansible.sh

--- Terraform/install-ansible.sh
+++ Terraform/install-ansible.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
-sudo apt-get install ansible -y
+sudo apt-get install python-pip -y
+sudo pip install ansible
 ansible-galaxy install PaloAltoNetworks.paloaltonetworks
 wget https://pypi.python.org/packages/12/f8/63bcaa57be2c6c9b1c18035316463ae211e1d6b0ec98146eebe48af4f80f/pan-python-0.12.0.tar.gz
 sudo tar xzf pan-python*.tar.gz
 wget https://pypi.python.org/packages/da/63/ebb7c707ecea80454384983c9c0eaf718667a7e9f72bc453efe00ffa3536/pandevice-0.5.0.tar.gz
 sudo tar xzf pandevice*.tar.gz
-sudo apt install python-xmltodict -y
-sudo apt-get install python-pip -y
+pip install xmltodict
